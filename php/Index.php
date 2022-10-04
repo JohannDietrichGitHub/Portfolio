21c54d3a4ffbfd5fa4ec3d9ac924d32f2edda8a9
jojohub69 <jodiet@hotmail.fr>
2022-10-04 16:18:58 +0200

    forme

--- php/Index.php
+++ php/Index.php
@@ -1,4 +1,7 @@
-<head> titre</head>
+<head> 		
+    <meta charset="utf-8" /> <!--encodage en utf8-->
+	<title>Contact</title>
+</head>
 <body>
 Bonjour, ceci est un test pour github
 <div class="premier block titre"> PORTFOLIO JOHANN DIETRICH ! </div>
