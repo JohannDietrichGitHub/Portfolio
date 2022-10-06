8acedd043c380fc26db17fb4db294d6979b86195
jojohub69 <jodiet@hotmail.fr>
2022-10-06 08:53:49 +0200

    a

--- php/Index.php
+++ php/Index.php
@@ -6,5 +6,13 @@
     <script src="..\JS\indexjs.js"></script>
 </head>
 <body>
+<div class="card text-center">
+  <div class="card-header">
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+</div>  
 <div class="premierblocktitre"> PORTFOLIO JOHANN DIETRICH !</div>
 </body>
