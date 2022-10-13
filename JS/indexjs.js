2806012d58612994ee7b7cbca0b3fa0e04681f0d
jojohub69 <jodiet@hotmail.fr>
2022-10-13 10:44:00 +0200

    login + admin cv

--- JS/indexjs.js
+++ JS/indexjs.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 var modal = document.getElementById("myModal");
 
@@ -8,32 +5,14 @@ var btn = document.getElementById("myBtn");
 
 var span = document.getElementsByClassName("fermer")[0];
 
-=======
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
->>>>>>> bbb0e2d (a)
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-<<<<<<< HEAD
-=======
-// When the user clicks on <span> (x), close the modal
->>>>>>> bbb0e2d (a)
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-<<<<<<< HEAD
 
 
 function reveal() {
@@ -54,17 +33,3 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
-=======
-alert("ok");
->>>>>>> 133052e (mise en place des fichiers js css)
-=======
-/* alert("ok"); */
->>>>>>> 6caa6c9 (up)
-=======
-// When the user clicks anywhere outside of the modal, close it
-/* window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} */
->>>>>>> bbb0e2d (a)
--- php/Index.php
+++ php/Index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // Initialize the session
 session_start();
@@ -34,57 +33,4 @@ session_start();
   require_once('cv.php');
   ?>
   <a href="index.php">remonter</a>
-=======
-<head> 		
-    <meta charset="utf-8" /> <!--encodage en utf8-->
-	<title>Contact</title>
-    <link rel="stylesheet" href="..\CSS\indexcss.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="..\JS\indexjs.js"></script>
-</head>
-<body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-Bonjour, ceci est un test pour github
-<<<<<<< HEAD
-<<<<<<< HEAD
-<div class="premier block titre"> PORTFOLIO JOHANN DIETRICH ! </div>
->>>>>>> 21c54d3 (forme)
-=======
-<div class="premierblocktitre"> PORTFOLIO JOHANN DIETRICH ! </div>
->>>>>>> e1452f4 (upadte files)
-=======
-=======
->>>>>>> 6caa6c9 (up)
-=======
-<div class="card text-center">
-  <div class="card-header">
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-  </div>
-</div>  
->>>>>>> 8acedd0 (a)
-<div class="premierblocktitre"> PORTFOLIO JOHANN DIETRICH !</div>
->>>>>>> 133052e (mise en place des fichiers js css)
-=======
-  <?php
-    require_once ('navbar.php');
-  ?>
-  <div class="card text-center">
-    <div class="card-header">
-    </div>
-    <div class="card-body">
-      <h5 class="card-title">Special title treatment</h5>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    </div>
-  </div>  
-  <div class="premierblocktitre"> PORTFOLIO JOHANN DIETRICH !</div>
-  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-  <?php
-    require_once ('formtest.php');
-  ?>
->>>>>>> bbb0e2d (a)
 </body>
