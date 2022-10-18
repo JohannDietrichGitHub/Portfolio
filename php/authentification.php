1e9d5a7347dc3636a6dc2d161e473e8a67272f02
jojohub69 <jodiet@hotmail.fr>
2022-10-18 11:31:12 +0200

    Finition du système empêchant 2 même noms d'utilistaeurs/adresses mail

--- php/authentification.php
+++ php/authentification.php
@@ -59,19 +59,33 @@ catch(PDOException $e)
 
 
 
-
-$usernamebdd = $mailbdd = ""; /* a changer, doit chercher dans bdd si ils existent ou pas */
-
 if(isset($_POST['username'])){   /* vérifie si le formulaire a été envoyé */
+
+    $stmt = $conn->prepare("SELECT username FROM users WHERE username=:username");
+    $stmt->execute(['username' => $_POST["username"]]); 
+    $user = $stmt->fetch();
+    if (empty($user)){
+        $usernamebdd = "";
+    }
+    else { $usernamebdd = $user[0]; }
+
+
+    $stmt = $conn->prepare("SELECT mail FROM users WHERE mail=:mail");
+    $stmt->execute(['mail' => $_POST["mail"]]); 
+    $user = $stmt->fetch();
+    if (empty($user)){
+        $mailbdd = "";
+    }
+    else { $mailbdd = $user[0]; }
 
     if($_POST['password']== $_POST['confpassword']){ /* vérifie si les mots de passes sont les memes */
         if ($_POST['username']== $usernamebdd || $_POST['mail']== $mailbdd) {  /* vérifie si le nom d'utilisateur ou l'adresse mail ne sont pas déja utilisés */
             echo "nom d'utilisateur ou adresse mail déja utilisés";
         }
         else {
-        $mdphash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $sql = "INSERT INTO users (username, mail, password) VALUES (?,?,?)";
-        $conn->prepare($sql)->execute([$_POST['username'], $_POST['mail'], $mdphash]);
+            $mdphash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (username, mail, password) VALUES (?,?,?)";
+            $conn->prepare($sql)->execute([$_POST['username'], $_POST['mail'], $mdphash]);
         }
     }
     else {
